0a9b7e62b3f7a154ff09d0d3d3e13ba774331329
Meghana T D <meghanatd.inbox@gmail.com>
2026-06-01 14:07:09 +0530

    Created simple_calculator.py
    Uploaded a Python calculator program that performs basic arithmetic operations including addition, subtraction, multiplication, and division using user-defined functions and menu-driven input.

---
+++ simple_calculator.py
@@ -0,0 +1,24 @@
+def add(x,y):
+  return x + y
+def subtract(x,y):
+  return x - y
+def multiply(x,y):
+  return x * y
+def divide(x,y):
+  return x / y
+#menu
+print("Select operation: ")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+ if choice =='1':
+   print(n1,"+",n2,"=",add(n1,n2))
+ elif choice =='2':
+   print(n1,"-",n2,"=",subtract(n1,n2))
+ elif choice =='3':
+   print(n1,"×",n2,"=",multiply(n1,n2))
+ elif choice =='4':
+   print(n1,"÷",n2,"=",divide(n1,n2))
+ else: 
+   print("Invalid! Enter choice between 1 to 4")
