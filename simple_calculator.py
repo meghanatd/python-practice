acabf526979b2f42d244c46fed2ededcfc42c3d0
Meghana T D <meghanatd.inbox@gmail.com>
2026-06-01 14:18:39 +0530

    Update simple_calculator.py

--- simple_calculator.py
+++ simple_calculator.py
@@ -5,6 +5,8 @@ def subtract(x,y):
 def multiply(x,y):
   return x * y
 def divide(x,y):
+  if y==0:
+    return "Cannot divide by 0"
   return x / y
 #menu
 print("Select operation: ")
@@ -12,6 +14,9 @@ print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
+choice=input("Enter a choice (1 to 4)")
+n1=float(input("Enter first number: ")
+n2=float(input("Enter second number: ")
  if choice =='1':
    print(n1,"+",n2,"=",add(n1,n2))
  elif choice =='2':
